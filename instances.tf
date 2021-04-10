629629f7312a42958c92383aabb67b38e15c917e
root <root@rhel-8.2>
2021-04-11 04:46:52 +0530

    adding instance.tf

--- instances.tf
+++ instances.tf
@@ -4,7 +4,7 @@ data "aws_ssm_parameter" "ami-master" {
 }
 data "aws_ssm_parameter" "ami-worker" {
   provider = aws.region-worker
-  name     = "/aws/service/ami-amazon-linux-latest/amzn-ami-hvm-x86_64-gp2" 
+  name     = "/aws/service/ami-amazon-linux-latest/amzn-ami-hvm-x86_64-gp2"
 }
 
 resource "aws_key_pair" "master-key" {
@@ -31,6 +31,28 @@ resource "aws_instance" "jenkins-master" {
   tags = {
     Name = "jenkins-master"
   }
+
+  provisioner "remote-exec" {
+    inline = ["sudo yum install python -y"]
+
+    connection {
+      host        = self.public_ip # The `self` variable is like `this` in many programming languages
+      type        = "ssh"          # in this case, `self` is the resource (the server).
+      user        = "ec2-user"
+      private_key = file("~/.ssh/id_rsa")
+    }
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      >master.ini;
+       echo "[master]" | tee -a master.ini;
+       echo "${self.public_ip}" | tee -a  master.ini;
+      export ANSIBLE_HOST_KEY_CHECKING=False;
+          ansible-playbook -u ec2-user --private-key ${var.private_key} -i  master.ini ansible-template/jenkins-master.yml
+    EOT
+  }
+
 }
 
 # Jenkins worker in subnet 1 us-west-2
@@ -47,4 +69,29 @@ resource "aws_instance" "jenkins-worker" {
   tags = {
     Name = join("_", ["jenkins-worker", count.index + 1])
   }
+
+  provisioner "remote-exec" {
+    inline = ["sudo yum install python -y"]
+
+    connection {
+      host        = self.public_ip # The `self` variable is like `this` in many programming languages
+      type        = "ssh"          # in this case, `self` is the resource (the server).
+      user        = "ec2-user"
+      private_key = file("~/.ssh/id_rsa")
+    }
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+       >worker.ini;
+        echo "[worker]" | tee -a worker.ini;
+        echo "${self.public_ip}" | tee -a worker.ini;
+      export ANSIBLE_HOST_KEY_CHECKING=False;
+          ansible-playbook -u ec2-user --private-key ${var.private_key} -i worker.ini ansible-template/jenkins-worker.yml
+    EOT
+  }
+
 }
+
+
+
--- variables.tf
+++ variables.tf
@@ -8,10 +8,32 @@ variable "worker" {
 }
 variable "peer_owner_id" {
   type    = string
-  default = "967756262698"
+  default = "771680087539"
 }
 
 variable "worker-count" {
   type    = number
   default = 1
 }
+
+variable "region-master" {
+  type    = string
+  default = "us-east-1"
+}
+variable "region-worker" {
+  type    = string
+  default = "us-west-2"
+}
+variable "profile" {
+  type    = string
+  default = "default"
+}
+variable "ansible_user" {
+  type    = string
+  default = "ec2-user"
+}
+variable "private_key" {
+  type    = string
+  default = "~/.ssh/id_rsa"
+}
+
